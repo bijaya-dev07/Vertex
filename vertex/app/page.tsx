2f71698e2330399baa517e87595ccdda69806185
kojing777 <kojingmoktan92@gmail.com>
2026-01-16 21:43:56 +0545

    feat: update Home page description and add Badge component

--- vertex/app/page.tsx
+++ vertex/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Share What You Know, Discover what You Don&apos;t.</h1>
-      <p>A place to learn and grow together.</p>
+      <p>A place to learn and grow together and share knowledge.</p>
       <Button>Get Started</Button>
       <Button variant="outline">Learn More</Button>
     </div>
