ccb53819e8ffb08d288670afc9730135d94b3bb4
kojing777 <kojingmoktan92@gmail.com>
2026-01-16 21:42:20 +0545

    feat: update Home page content and button variants

--- vertex/app/page.tsx
+++ vertex/app/page.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Hi its Bijay Tamang</h1>
-      <Button variant="destructive">Click Me</Button>
+      <h1>Share What You Know, Discover what You Don&apos;t.</h1>
+      <p>A place to learn and grow together.</p>
+      <Button>Get Started</Button>
+      <Button variant="outline">Learn More</Button>
     </div>
   );
 }
