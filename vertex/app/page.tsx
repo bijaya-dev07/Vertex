b4fc12fd806aca866a19272badf6efa285cd7462
kojing777 <kojingmoktan92@gmail.com>
2026-01-17 22:28:50 +0545

    feat: add FeaturedProjects component and integrate it into Home page

--- vertex/app/page.tsx
+++ vertex/app/page.tsx
@@ -1,7 +1,11 @@
+import FeaturedProjects from "@/components/ui/landing-page/featured-projects";
 import HeroSection from "@/components/ui/landing-page/hero-section";
 
 export default function Home() {
   return (
+    <div>
    <HeroSection />
+   <FeaturedProjects />
+   </div>
   );
 }
