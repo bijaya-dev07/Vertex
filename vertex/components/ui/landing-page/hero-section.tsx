9a5dab3028d9b444a9be27ab6478267954fdc263
kojing777 <kojingmoktan92@gmail.com>
2026-01-17 20:56:45 +0545

    feat: enhance HeroSection with improved layout and navigation links

--- vertex/components/ui/landing-page/hero-section.tsx
+++ vertex/components/ui/landing-page/hero-section.tsx
@@ -1,5 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function HeroSection() {
   const LiveBadge = () => {
@@ -13,7 +14,9 @@ export default function HeroSection() {
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>{" "}
           <span className="relative inline-flex rounded-full h-2 w-2 bg-primary"></span>
         </span>
-        Join Thousands of creatots sharing their works.
+        <span className="text-muted-foreground">
+          Join Thousands of creatots sharing their works.
+        </span>
       </Badge>
     );
   };
@@ -24,24 +27,29 @@ export default function HeroSection() {
           <div className="flex flex-col items-center justify-center lg:py-24 py-12 text-center">
             <LiveBadge />
 
-            <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+            <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold tracking-tight leading-tight mb-6 max-w-5xl">
               Launch your product.
               <br />
-              Get discovered.
-              <br />
-              Grow with the community.
+              Get discovered. Grow with the community.
             </h1>
 
-            <p className="text-muted-foreground text-lg">
+            <p className="text-muted-foreground text-lg sm:text-xl mb-10 max-w-2xl leading-relaxed">
               Vertex is a modern product launch platform where creators showcase
               apps, AI tools, SaaS products, and side projects. Get real
               feedback, community votes, and visibility — all in one place.
             </p>
 
-            <div className="flex gap-4">
-              <Button size="lg">Get Started</Button>
-              <Button size="lg" variant="outline">
-                Explore Products
+            <div className="flex flex-col sm:flex-row gap-4 mb-16">
+              <Button asChild className="text-base px-6 shadow-lg" size="lg">
+                <Link href="/submit">Get Started</Link>
+              </Button>
+              <Button
+                variant="secondary"
+                asChild
+                className="text-base px-6 shadow-lg"
+                size="lg"
+              >
+                <Link href="/submit">Get Started</Link>
               </Button>
             </div>
           </div>
