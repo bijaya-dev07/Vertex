438c37ae914342ad114b3b2ff768a2b1545add24
kojing777 <kojingmoktan92@gmail.com>
2026-01-21 23:29:56 +0545

    feat: refactor ProductCard layout for improved structure and styling

--- vertex/components/products/product-cards.tsx
+++ vertex/components/products/product-cards.tsx
@@ -20,20 +20,29 @@ interface product {
 export default function ProductCard({ product }: { product: product }) {
   return (
     <Link href={`/products/${product.id}`}>
-      <Card className="group card-hover hover:bg-primary-foreground/10">
-        <CardTitle>{product.name}</CardTitle>
-        {product.isFeatuered && <Badge className="mb-2">Featured</Badge>}
-        <CardDescription>{product.description}</CardDescription>
-
+      <Card className="group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400">
         <CardHeader>
-          <CardFooter>
+          <div className="flex items-center gap-4">
+            <div className="flex-1 min-w-0">
+              <div className="flex items-center gap-2">
+                <CardTitle>{product.name}</CardTitle>
+                {product.isFeatuered && (
+                  <Badge className="mb-2">Featured</Badge>
+                )}
+              </div>
+              <CardDescription>{product.description}</CardDescription>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex items-center gap-2">
             {product.tags.map((tag) => (
               <Badge variant="secondary" key={tag}>
                 {tag}
               </Badge>
             ))}
-          </CardFooter>
-        </CardHeader>
+          </div>
+        </CardFooter>
       </Card>
     </Link>
   );
