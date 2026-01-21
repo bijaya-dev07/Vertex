b707f6c45798769480d01b65e894c182930f2114
kojing777 <kojingmoktan92@gmail.com>
2026-01-21 21:02:13 +0545

    feat: enhance SectionHeader component with icon and description; update FeaturedProjects to utilize new props

--- vertex/components/common/section-header.tsx
+++ vertex/components/common/section-header.tsx
@@ -1,9 +1,21 @@
-export default function SectionHeader({ title }: { title: string }) {
+import { LucideIcon } from "lucide-react";
+
+export default function SectionHeader({
+  title,
+  icon: Icon,
+  description,
+}: {
+  title: string;
+  icon: LucideIcon;
+  description: string;
+}) {
   return (
     <div className="mb-8">
       <div className="items-center flex gap-2 mb-3">
-        <h2 className="text-2xl font-bold">{title}</h2>
+        <Icon className="size-6 text-primary" />
+        <h2 className="text-3xl text-primary font-bold">{title}</h2>
       </div>
+      <p className="text-muted-foreground text-lg">{description}</p>
     </div>
   );
 }
--- vertex/components/ui/landing-page/featured-projects.tsx
+++ vertex/components/ui/landing-page/featured-projects.tsx
@@ -1,12 +1,24 @@
 import SectionHeader from "@/components/common/section-header";
+import { ArrowRightIcon, Star } from "lucide-react";
+import { Button } from "../button";
+import Link from "next/link";
 
 export default function FeaturedProjects() {
   return (
     <div>
-      <section className="py-20 bg-muted/20">
+      <section className="px-20 bg-muted/20">
         <div className="wrapper">
-          <div className="flex items-center justify-between mb-12">
-            <SectionHeader title="Featured Projects" />
+          <div className="flex items-center justify-between mb-8">
+            <SectionHeader
+              title="Featured Today"
+              icon={Star}
+              description="Check out the projects featured today."
+            />
+            <Button variant="outline" asChild className="hidden sm:flex">
+              <Link href="/explore/projects">
+              See All <ArrowRightIcon className="ml-2 size-4" />
+              </Link>
+            </Button>
           </div>
         </div>
       </section>
